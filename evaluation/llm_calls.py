ec0946f9e30b4ff265bc6c252588a3cb765fccc0
Ying Zhang <lynnyzhang@gmail.com>
2026-08-17 23:51:15 -0400

    open telemetry panel

--- evaluation/llm_calls.py
+++ evaluation/llm_calls.py
@@ -3,31 +3,6 @@ import time
 from src.llm_client import LLMClient
 
 
-def calc_price(usage):
-    input_price_per_million = 0.75
-    output_price_per_million = 4.50
-
-    input_cost = (usage.input_tokens / 1_000_000) * input_price_per_million
-    output_cost = (usage.output_tokens / 1_000_000) * output_price_per_million
-    total_cost = input_cost + output_cost
-
-    return {
-        "input_cost": input_cost,
-        "output_cost": output_cost,
-        "total_cost": total_cost,
-    }
-
-
-def calc_total_price(usages):
-    total_cost = 0.0
-
-    for usage in usages:
-        cost = calc_price(usage)
-        total_cost = total_cost + cost["total_cost"]
-
-    return total_cost
-
-
 def llm_structured(client, instructions, user_prompt, output_type, model=None):
     model = model or LLMClient.get_model()
     messages = [
